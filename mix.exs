5ca383c645dfafe5cc8a0aed0d626e3e3960664b
davalbagr <davalbagr2002@gmail.com>
2021-07-23 01:53:00 +0100

    migrated from python script to elixir mix task

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule EgggenApi.MixProject do
   def application do
     [
       mod: {EgggenApi.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :inets]
     ]
   end
 
@@ -34,12 +34,12 @@ defmodule EgggenApi.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.7"},
-      {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:progress_bar, "> 0.0.0"}
     ]
   end
 
