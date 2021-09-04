b7be5bf4d96bb6d18ef72c48d13ea65ecf66bfa3
davalbagr <davalbagr2002@gmail.com>
2021-09-04 02:24:06 +0100

    improved py script

--- gen_json.py
+++ gen_json.py
@@ -1,14 +1,12 @@
-from simplejson import dumps, loads
-import aiohttp, asyncio
+from json import dumps, loads
+from urllib.request import urlopen, Request
 
 
-async def get(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return loads(await response.content.read())
+def get(url):
+    return loads(urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'})).read())
 
 
-async def fetch_moves(r):
+def fetch_moves(r):
     egg_moves = []
     normal_moves = []
     for i in r:
@@ -22,11 +20,11 @@ async def fetch_moves(r):
     return normal_moves, egg_moves
 
 
-async def fetch_abilities(r2):
+def fetch_abilities(r2):
     normal_abilities = []
     hidden_abilities = []
     for abilities in r2:
-        r3 = await get(abilities['ability']['url'])
+        r3 = get(abilities['ability']['url'])
         if abilities['is_hidden']:
             hidden_abilities.append(
                 {'id': r3['id'], "gen": r3['generation']['name']})
@@ -37,27 +35,22 @@ async def fetch_abilities(r2):
     return normal_abilities, hidden_abilities
 
 
-async def fetch_pokemon(json):
+def fetch_pokemon(json):
     rtrnval = []
     for i in json:
-        r = await get(i['url'])
+        r = get(i['url'])
         if r['evolves_from_species'] is None and not r['is_legendary'] and not r['is_mythical']:
-            r2 = await get("https://pokeapi.co/api/v2/pokemon/{0}".format(r['id']))
-            (normal_moves, egg_moves) = await fetch_moves(r2['moves'])
-            (normal_abilities, hidden_abilities) = await fetch_abilities(r2['abilities'])
+            r2 = get("https://pokeapi.co/api/v2/pokemon/{0}".format(r['id']))
+            (normal_moves, egg_moves) = fetch_moves(r2['moves'])
+            (normal_abilities, hidden_abilities) = fetch_abilities(r2['abilities'])
             rtrnval.append({'pokemon_id': r['id'], 'normal_moves': normal_moves,
                             'egg_moves': egg_moves, 'normal_abilities': normal_abilities,
                             'hidden_abilities': hidden_abilities, 'pokemon_gen': r['generation']['name']})
             print(r['id'])
     return rtrnval
 
-
-async def main():
-    json = await get("https://pokeapi.co/api/v2/pokemon-species?limit=10000")
-    with open("priv/pokemons.json", "w") as f:
-        pokemons = await fetch_pokemon(json['results'])
-        f.write(dumps(pokemons))
-
-
 if __name__ == '__main__':
-    asyncio.run(main())
+    json = get("https://pokeapi.co/api/v2/pokemon-species?limit=10000")
+    with open("priv/pokemons.json", "w") as f:
+        pokemons = fetch_pokemon(json['results'])
+        f.write(dumps(pokemons))
