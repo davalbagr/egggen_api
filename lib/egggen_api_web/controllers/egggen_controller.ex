8c7a393e075fcd68decd1250f489fcf82fc20962
davalbagr <davalbagr2002@gmail.com>
2021-09-04 18:36:32 +0100

    added exception for requesting more than 10000 pokemons to be generated

--- lib/egggen_api_web/controllers/egggen_controller.ex
+++ lib/egggen_api_web/controllers/egggen_controller.ex
@@ -4,12 +4,20 @@ defmodule EgggenApiWeb.EgggenController do
   @file_data File.read!(Application.app_dir(:egggen_api, "priv/pokemons.json")) |> Jason.decode!()
 
   def index(conn, %{"numb_to_gen" => numb_to_gen, "game" => game, "egg_move_chance" => egg_move_chance, "hidden_ability_chance" => hidden_ability_chance, "shiny_chance" => shiny_chance}) do
-    pokemons = @file_data |> EgggenApi.gen_pokemons(String.to_integer(numb_to_gen), game, String.to_integer(egg_move_chance), String.to_integer(hidden_ability_chance), String.to_integer(shiny_chance), false)
+    pokemons = if String.to_integer(numb_to_gen) < 10001 do
+                @file_data |> EgggenApi.gen_pokemons(String.to_integer(numb_to_gen), game, String.to_integer(egg_move_chance), String.to_integer(hidden_ability_chance), String.to_integer(shiny_chance), false)
+               else
+                json(conn, %{"error" => "requested too many eggs to be generated"})
+               end
     json(conn, pokemons)
   end
 
   def index2(conn, %{"numb_to_gen" => numb_to_gen, "game" => game, "egg_move_chance" => egg_move_chance, "hidden_ability_chance" => hidden_ability_chance, "shiny_chance" => shiny_chance}) do
-    pokemons = @file_data |> EgggenApi.gen_pokemons(String.to_integer(numb_to_gen), game, String.to_integer(egg_move_chance), String.to_integer(hidden_ability_chance), String.to_integer(shiny_chance), true)
+    pokemons = if String.to_integer(numb_to_gen) < 10001 do
+                @file_data |> EgggenApi.gen_pokemons(String.to_integer(numb_to_gen), game, String.to_integer(egg_move_chance), String.to_integer(hidden_ability_chance), String.to_integer(shiny_chance), true)
+               else
+                json(conn, %{"error" => "requested too many eggs to be generated"})
+               end
     json(conn, pokemons)
   end
 end
